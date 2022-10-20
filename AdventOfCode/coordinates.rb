9e68d66081cb27ecaf72afc22157deae99bdd30d
3rick <erick.dejesus1@alumno.buap.mx>
2022-10-20 17:46:16 -0500

    add: coordinates

--- AdventOfCode/coordinates.rb
+++ AdventOfCode/coordinates.rb
@@ -1,13 +1,19 @@
 class Coordinates
-    
+
     def initialize(param, casas)
         @param = param.split("")
         @x = 0
         @y = 0
         @casas = casas
-    
+        @santa = []
+        @robot = []
+
     end
 
+    #< x.(-1)
+    #> x.(+1)
+    #v  y.(-1)
+    #^ y.(+1)
     def step(p)
         if p == '>'
             @x+=1
@@ -21,21 +27,38 @@ class Coordinates
         return [@x,@y]
     end
 
-    def recorrido
+    def recorridoSanta
         @param.each do |p|
             @casas << step(p)
         end
-        p @casas.uniq.size
+        return @casas.uniq.size()
     end
-    
-    
+    def recorridoSanta_Robot
+        @param.each_with_index do |p,idx|
+            if idx % 2 == 0
+                @santa << p
+            else
+                @robot << p
+            end
+        end
+        @santa.each do |p|
+            @casas << step(p)
+        end
+        @x = 0
+        @y = 0
+        @robot.each do |p|
+            @casas << step(p)
+        end
+        return @casas.uniq.size()
+
+    end
 end
-#< x.(-1) 
-#> x.(+1)
-#v  y.(-1)
-#^ y.(+1)
+
 
 if __FILE__ == $0
-    test1 = Coordinates.new("^v^v^v^v^v", [[0,0]])
-    test1.recorrido
+
+    # input = File.read("input.txt")
+    # puts input.split("")
+    test1 = Coordinates.new("^v", [[0,0]])
+    p test1.recorridoSanta_Robot
 end
