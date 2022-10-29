fc6354a7713e4a70b63243ec42b4a28958a118f5
3rick <erick.dejesus1@alumno.buap.mx>
2022-10-29 18:13:07 -0500

    test: add part II AOC 05

--- AdventOfCode/05/naughtyStrings_spec.rb
+++ AdventOfCode/05/naughtyStrings_spec.rb
@@ -1,7 +1,7 @@
 require './naughtyStrings.rb'
 
 RSpec.describe NaughtyStrings do
-  context "Given a string, determine wheather it is nice or naughty"do
+  context "A string is nice if. It contains at least three vowels, at least one letter that appears twice in a row, does not contain the strings ab, cd, pq, or xy"do
     it"jchzalrnumimnmhp is naughty (no double letter)"do
       expect(NaughtyStrings.nice?("jchzalrnumimnmhp")).to eq(false)
     end
@@ -16,6 +16,20 @@ RSpec.describe NaughtyStrings do
     end
     it"aaa is nice"do
       expect(NaughtyStrings.nice?("aaa")).to eq(true)
+    end
+  end
+  context "A string is nice if it has two letters that appear at least twice without overlaping. And one letter which repeats with exactly one letter between them" do
+    it "qjhvhtzxzqqjkmpb is nice" do
+      expect(NaughtyStrings.nicer?("qjhvhtzxzqqjkmpb")).to eq(true)
+    end
+    it "xxyxx is nice" do
+      expect(NaughtyStrings.nicer?("xxyxx")).to eq(true)
+    end
+    it "uurcxstgmygtbstg is naughty" do
+      expect(NaughtyStrings.nicer?("uurcxstgmygtbstg")).to eq(false)
+    end
+    it "ieodomkazucvgmuy is naughty" do
+      expect(NaughtyStrings.nicer?("ieodomkazucvgmuy")).to eq (false)
     end
   end
 end
