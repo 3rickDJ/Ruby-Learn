81b9f97fe65958d5f6cd3546fb69f88b3f73f2c3
3rick <erick.dejesus1@alumno.buap.mx>
2022-10-29 18:13:53 -0500

    add: part II AOC 05

--- AdventOfCode/05/naughtyStrings.rb
+++ AdventOfCode/05/naughtyStrings.rb
@@ -5,13 +5,17 @@ class NaughtyStrings
     badTuples = !text.match(/(?:ab)|(?:cd)|(?:pq)|(?:xy)/)
     badTuples && threeVowels && twice
   end
+  def self.nicer?(text)
+    pairTwice = !!text.match(/([a-zA-Z][a-zA-Z]).*\1/)
+    oneBetweenTwo = !!text.match(/(\w).\1/)
+    pairTwice && oneBetweenTwo
+  end
 end
 
 if __FILE__ == $0
   strings = File.readlines("partI.txt")
-  counter = 0
   strings = strings.collect do |line|
-    NaughtyStrings.nice?(line) ? 1:0
+    NaughtyStrings.nicer?(line) ? 1:0
   end
   p strings.sum
 end
